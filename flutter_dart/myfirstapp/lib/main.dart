58392b861187df8b56d90093b14697ba34f75274
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-01-12 15:27:35 +0000

    create views,widget dir in the lib dir in the myfirstapp dirin flutter_dart dir

--- flutter_dart/myfirstapp/lib/main.dart
+++ flutter_dart/myfirstapp/lib/main.dart
@@ -1,42 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:myfirstapp/widgets/navbar_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-//stateless
-//material app
+/// key things in  flutter
+//material app(statefull widget)
 //scaffold
+//app bar
+// navigation
 
+//states
+/// statefull === screen can refresh
+/// stateless === screen cannot refresh
+/// setstate == telling it to refresh
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
-          brightness: Brightness.dark,
-        )
-      ),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal, brightness: Brightness.dark)),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App',
-          style: TextStyle(color: Colors.red),),
-          centerTitle: true,
-          // leading: Icon(Icons.login),
-          // backgroundColor: Colors.white,
-        ),
-        bottomNavigationBar: NavigationBar(destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.mic), label: 'Mic')
-        ],
-        onDestinationSelected: (int value) => {},
-        selectedIndex: 0,
-        ),
+        appBar: AppBar(title: Text('Flutter Map')),
+        bottomNavigationBar:NavbarWidget(),
       ),
     );
   }
---
+++ flutter_dart/myfirstapp/lib/widgets/navbar_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NavbarWidget extends StatefulWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  State<NavbarWidget> createState() => _NavbarWidgetState();
+}
+
+class _NavbarWidgetState extends State<NavbarWidget> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+      ],
+      onDestinationSelected: (value) => {
+        setState(() {
+          selectedIndex = value;
+        }),
+      },
+      selectedIndex: selectedIndex,
+    );
+  }
+}
